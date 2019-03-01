3c4b6b8283f24cb515da21acaf418cafbf1cc2bc
Arnolds Pukss <pukss.arnolds@gmail.com>
2019-03-01 12:30:31 +0200

    init

---
+++ src/test/java/com/example/simplespringweb/SimpleSpringWebApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.simplespringweb;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SimpleSpringWebApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
